8ae82a0a79a2208d43df8b93dd1a963e85ffa506
Stefano Stirati <stefanos@Mac-mini-di-Stefano.local>
2016-01-15 08:58:51 +0100

    Menu and logo added

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -38,8 +38,8 @@
 				<a href="#" id="btn-nav-sidebar-minified" class="btn btn-link btn-nav-sidebar-minified pull-left"><i class="icon ion-arrow-swap"></i></a>
 				<a class="btn btn-link btn-off-canvas pull-left"><i class="icon ion-navicon"></i></a>
 				<div class="logo pull-left">
-					<a href="index.html">
-						<img src={{ asset('assets/img/queenadmin-logo.png') }} alt="QueenAdmin Logo" />
+					<a href="{{URL::to('home')}}">
+						<img src={{ asset('assets/img/logos_glamore_big.png') }} alt="Glamore Logo" />
 					</a>
 				</div>
 			</div>
@@ -131,7 +131,7 @@
 				<div class="logged-user">
 					<div class="btn-group">
 						<a href="#" class="btn btn-link dropdown-toggle" data-toggle="dropdown">
-							<img src={{ asset('assets/img/user-loggedin.png') }} alt="Sebastian" /><span class="name">Sebastian <i class="icon ion-ios-arrow-down"></i></span>
+							<img src={{ asset('assets/img/user-loggedin.png') }} alt="Sebastian" /><span class="name">{{Auth::user()->name}} <i class="icon ion-ios-arrow-down"></i></span>
 						</a>
 						<ul class="dropdown-menu" role="menu">
 							<li>
@@ -174,90 +174,43 @@
 						<li class="has-submenu active">
 							<a href="#" class="submenu-toggle"><i class="icon ion-ios-speedometer-outline"></i><span class="text">Dashboards</span></a>
 							<ul class="list-unstyled sub-menu collapse in">
-								<li class="active"><a href="index.html"><span class="text">Dashboard v1</span></a></li>
-								<li><a href="index2.html"><span class="text">Dashboard v2</span></a></li>
+								<li class="active"><a href="{{URL::to('home')}}"><span class="text">Dashboard</span></a></li>
+								<li><a href="index2.html"><span class="text">Next meetings</span></a></li>
+								<li><a href="index2.html"><span class="text">Task to do</span></a></li>
+								<li><a href="index2.html"><span class="text">Card of the last project</span></a></li>
+								<li><a href="index2.html"><span class="text">Chart of earnings</span></a></li>
 							</ul>
 						</li>
 						<li class="has-submenu">
-							<a href="#" class="submenu-toggle"><i class="icon ion-ios-paper-outline"></i><span class="text">Forms</span></a>
+							<a href="#" class="submenu-toggle"><i class="icon ion-ios-paper-outline"></i><span class="text">Projects</span></a>
 							<ul class="list-unstyled sub-menu collapse">
-								<li><a href="form-fancy-elements.html"><span class="text">Fancy Elements</span></a></li>
-								<li><a href="form-inplace-editing.html"><span class="text">In-place Editing</span></a></li>
-								<li><a href="form-bootstrap-elements.html"><span class="text">Bootstrap Elements</span></a></li>
-								<li><a href="form-layouts.html"><span class="text">Form Layouts</span></a></li>
-								<li><a href="form-validations.html"><span class="text">Validation</span></a></li>
-								<li><a href="form-upload.html"><span class="text">File Upload</span></a></li>
-								<li><a href="form-text-editor.html"><span class="text">Text Editor</span></a></li>
-								<li><a href="form-wizard.html"><span class="text">Wizards</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">Post a project</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">My project</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">Activities (task/meeting/call)</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">Documents and note</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">Videoconference</span></a></li>
 							</ul>
 						</li>
+
+
+
 						<li class="has-submenu">
-							<a href="#" class="submenu-toggle"><i class="icon ion-ios-flask-outline"></i><span class="text">UI Elements</span></a>
+							<a href="#" class="submenu-toggle"><i class="icon ion-person"></i><span class="text">Affiliates</span></a>
 							<ul class="list-unstyled sub-menu collapse">
-								<li><a href="ui-elements-general.html"><span class="text">General</span></a></li>
-								<li><a href="ui-elements-buttons.html"><span class="text">Buttons</span></a></li>
-								<li><a href="ui-elements-tabs.html"><span class="text">Tabs</span></a></li>
-								<li><a href="ui-elements-tour.html"><span class="text">Tour</span></a></li>
-								<li><a href="ui-elements-icons.html"><span class="text">Icons</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">BIO Affiliate</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">Search for Affiliate</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">Activities diary</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">Search and join projects</span></a></li>
 							</ul>
 						</li>
-						<li><a href="widgets.html"><i class="icon ion-ios-color-wand-outline"></i><span class="text">Widgets</span></a></li>
+
 						<li class="has-submenu">
-							<a href="#" class="submenu-toggle"><i class="icon ion-ios-copy-outline"></i><span class="text">Pages</span></a>
+							<a href="#" class="submenu-toggle"><i class="icon ion-stats-bars"></i><span class="text">Reports</span></a>
 							<ul class="list-unstyled sub-menu collapse">
-								<li><a href="page-projects.html"><span class="text">Projects</span></a></li>
-								<li><a href="page-project-detail.html"><span class="text">Project Detail</span></a></li>
-								<li><a href="page-user-profile.html"><span class="text">Profile</span></a></li>
-								<li><a href="page-search-result.html"><span class="text">Search Result</span></a></li>
-								<li><a href="page-inbox.html"><span class="text">Inbox</span><span class='badge bg-orange'>12</span></a></li>
-								<li><a href="page-view-message.html"><span class="text">View Message</span></a></li>
-								<li><a href="page-new-message.html"><span class="text">New Message</span></a></li>
-								<li><a href="page-knowledgebase.html"><span class="text">Knowledge Base</span></a></li>
-								<li><a href="page-submit-ticket.html"><span class="text">Submit Ticket</span></a></li>
-								<li><a href="page-faq.html"><span class="text">FAQ</span></a></li>
-								<li><a href="page-pricing-tables.html"><span class="text">Pricing Tables</span></a></li>
-								<li><a href="page-invoice.html"><span class="text">Invoice</span></a></li>
-								<li><a href="page-register.html"><span class="text">Register</span></a></li>
-								<li><a href="page-login.html"><span class="text">Login</span></a></li>
-								<li><a href="page-login-alt.html"><span class="text">Login Alt.</span></a></li>
-								<li><a href="page-404.html"><span class="text">Not Found 404</span></a></li>
-								<li><a href="page-505.html"><span class="text">Error 505</span></a></li>
-								<li><a href="page-blank.html"><span class="text">Blank Page</span></a></li>
-							</ul>
-						</li>
-					</ul>
-					<h3>ESSENTIALS</h3>
-					<ul class="main-menu">
-						<li class="has-submenu">
-							<a href="#" class="submenu-toggle"><i class="icon ion-ios-pie-outline"></i><span class="text">Charts</span></a>
-							<ul class="list-unstyled sub-menu collapse">
-								<li class="active"><a href="charts-basic.html"><span class="text">Basic</span></a></li>
-								<li><a href="charts-interactive.html"><span class="text">Interactive Charts</span></a></li>
-							</ul>
-						</li>
-						<li class="has-submenu">
-							<a href="#" class="submenu-toggle"><i class="icon ion-ios-grid-view-outline"></i><span class="text">Tables</span></a>
-							<ul class="list-unstyled sub-menu collapse">
-								<li class="active"><a href="tables-static.html"><span class="text">Static Table</span></a></li>
-								<li><a href="tables-dynamic.html"><span class="text">Dynamic Table</span></a></li>
-							</ul>
-						</li>
-						<li><a href="maps.html"><i class="icon ion-ios-world-outline"></i><span class="text">Maps</span></a></li>
-						<li><a href="typography.html"><i class="icon ion-ios-compose-outline"></i><span class="text">Typography</span></a></li>
-						<li class="has-submenu">
-							<a href="#" class="submenu-toggle"><i class="icon ion-navicon"></i><span class="text">Menu Levels</span></a>
-							<ul class="list-unstyled sub-menu collapse">
-								<li class="has-submenu">
-									<a href="#" class="submenu-toggle"><span class="text">Second Lvl A</span></a>
-									<ul class="list-unstyled sub-menu collapse">
-										<li><a href="#"><span class="text">Third Lvl A1</span></a></li>
-										<li><a href="#"><span class="text">Third Lvl A2</span></a></li>
-										<li><a href="#"><span class="text">Third Lvl A3</span></a></li>
-									</ul>
-								</li>
-								<li><a href="#"><span class="text">Second Lvl B</span></a></li>
-								<li><a href="#"><span class="text">Second Lvl C</span></a></li>
-								<li><a href="#"><span class="text">Second Lvl D</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">payments received</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">summary of earnings</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">details of tax</span></a></li>
+								<li><a href="form-fancy-elements.html"><span class="text">invoice and remittance</span></a></li>
 							</ul>
 						</li>
 					</ul>
